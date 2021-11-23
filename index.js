e338f7f6747848eadd6e2eab03147f802d4e222a
jakubojcieszak <jakubojcieszak@gmail.com>
2021-11-23 23:22:03 +0100

    add scroll feature and margin in style

--- index.js
+++ index.js
@@ -2,9 +2,35 @@ document.addEventListener('DOMContentLoaded', function() {
     const rootElement = document.querySelector('#root');
 
     const sections = document.querySelectorAll('section');
+    let currentSectionIndex = 0;
+    let isThrottled = false;
 
     document.addEventListener('mousewheel', function(event) {
-        console.log(event.wheelDelta);
+
+        if (isThrottled) return;
+        isThrottled = true;
+
+        setTimeout(function(){
+            isThrottled = false;
+        },1000);
+
+        const direction = event.wheelDelta < 0 ? 1 : -1;
+
+        if(direction === 1){
+            const isLastSection = currentSectionIndex === sections.length -1;
+            if(isLastSection) return;
+        }
+        else if(direction === -1){
+            const firstSection = currentSectionIndex === 0;
+            if(firstSection) return;
+        }
+
+        currentSectionIndex = currentSectionIndex + direction;
+
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
     })
 
     
