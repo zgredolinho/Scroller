dccc8fc756655bcbfccf1ec1f5c3f60b2d87d869
jakubojcieszak <jakubojcieszak@gmail.com>
2021-11-24 23:53:34 +0100

    add key scroller function

--- index.js
+++ index.js
@@ -5,5 +5,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.addEventListener('swipeUp', () => scroller.scroll(1));
     document.addEventListener('swipeDown', () => scroller.scroll(-1));
+    document.addEventListener('keydown', (event) => {
+        
+        switch(event.key){
+           case "ArrowDown":
+               return scroller.scroll(1)
+            case "ArrowUp":
+                return scroller.scroll(-1)
+
+                default:
+                    return;
+        }
+
+    });
 
 })
