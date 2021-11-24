1a5967a683094097c8c29ce27c7e6449245ffac5
jakubojcieszak <jakubojcieszak@gmail.com>
2021-11-24 23:29:11 +0100

    Fix styles and add swiper founction

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     const scroller = new Scroller('#root');
-    document.addEventListener('wheel', (event) =>scroller.listenScroll(event))
+    document.addEventListener('wheel', (event) =>scroller.listenScroll(event));
+
+    document.addEventListener('swipeUp', () => scroller.scroll(1));
+    document.addEventListener('swipeDown', () => scroller.scroll(-1));
 
 })
